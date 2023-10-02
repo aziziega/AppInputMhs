be0f1a2d179c1ddaca84e472e310816423ba79b9
AzzCoding <aziziegatrimuthi16@gmail.com>
2023-10-02 15:45:17 +0700

    Tugas p4

---
+++ AplikasiInputDataMahasiswa/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace AplikasiInputDataMahasiswa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNim = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtKelas = new System.Windows.Forms.TextBox();
+            this.txtNilai = new System.Windows.Forms.TextBox();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lvwMahasiswa = new System.Windows.Forms.ListView();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnTampilkanData = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nim";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kelas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nilai";
+            // 
+            // txtNim
+            // 
+            this.txtNim.Location = new System.Drawing.Point(69, 7);
+            this.txtNim.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNim.Name = "txtNim";
+            this.txtNim.Size = new System.Drawing.Size(123, 22);
+            this.txtNim.TabIndex = 4;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(69, 38);
+            this.txtNama.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(189, 22);
+            this.txtNama.TabIndex = 5;
+            // 
+            // txtKelas
+            // 
+            this.txtKelas.Location = new System.Drawing.Point(69, 66);
+            this.txtKelas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtKelas.Name = "txtKelas";
+            this.txtKelas.Size = new System.Drawing.Size(123, 22);
+            this.txtKelas.TabIndex = 6;
+            // 
+            // txtNilai
+            // 
+            this.txtNilai.Location = new System.Drawing.Point(69, 100);
+            this.txtNilai.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtNilai.Name = "txtNilai";
+            this.txtNilai.Size = new System.Drawing.Size(123, 22);
+            this.txtNilai.TabIndex = 7;
+            this.txtNilai.TextChanged += new System.EventHandler(this.txtNilai_TextChanged);
+            this.txtNilai.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNilai_KeyPress);
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(14, 143);
+            this.btnSimpan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(71, 24);
+            this.btnSimpan.TabIndex = 8;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.btnSimpan_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(101, 143);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(79, 24);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lvwMahasiswa);
+            this.groupBox1.Controls.Add(this.btnHapus);
+            this.groupBox1.Controls.Add(this.btnTampilkanData);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(281, 10);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(690, 304);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[Daftar Mahasiswa]";
+            // 
+            // lvwMahasiswa
+            // 
+            this.lvwMahasiswa.HideSelection = false;
+            this.lvwMahasiswa.Location = new System.Drawing.Point(11, 56);
+            this.lvwMahasiswa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lvwMahasiswa.Name = "lvwMahasiswa";
+            this.lvwMahasiswa.Size = new System.Drawing.Size(659, 234);
+            this.lvwMahasiswa.TabIndex = 3;
+            this.lvwMahasiswa.UseCompatibleStateImageBehavior = false;
+            this.lvwMahasiswa.SelectedIndexChanged += new System.EventHandler(this.lvwMahasiswa_SelectedIndexChanged);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(140, 22);
+            this.btnHapus.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(78, 26);
+            this.btnHapus.TabIndex = 2;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnTampilkanData
+            // 
+            this.btnTampilkanData.Location = new System.Drawing.Point(13, 22);
+            this.btnTampilkanData.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTampilkanData.Name = "btnTampilkanData";
+            this.btnTampilkanData.Size = new System.Drawing.Size(121, 26);
+            this.btnTampilkanData.TabIndex = 1;
+            this.btnTampilkanData.Text = "Tampilkan Data";
+            this.btnTampilkanData.UseVisualStyleBackColor = true;
+            this.btnTampilkanData.Click += new System.EventHandler(this.btnTampilkanData_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(108, 48);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(7, 6);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 405);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.txtNilai);
+            this.Controls.Add(this.txtKelas);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtNim);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Aplikasi Input Data Mahasiswa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNim;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtKelas;
+        private System.Windows.Forms.TextBox txtNilai;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView lvwMahasiswa;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnTampilkanData;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AplikasiInputDataMahasiswa/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikasiInputDataMahasiswa
+{
+    
+    
+    public partial class Form1 : Form
+    {
+        private List<Mahasiswa> list = new List<Mahasiswa>();
+        public Form1()
+        {
+            InitializeComponent();
+            InisialisasiListView();
+        }
+        private void TampilkanData()
+        {
+            // kosongkan data listview
+            lvwMahasiswa.Items.Clear();
+            foreach(var mhs in list)
+            {
+                var noUrut = lvwMahasiswa.Items.Count + 1;
+
+                var item = new ListViewItem(noUrut.ToString());
+                item.SubItems.Add(mhs.Nim);
+                item.SubItems.Add(mhs.Nama);
+                item.SubItems.Add(mhs.Kelas);
+                item.SubItems.Add(mhs.Nilai.ToString());
+                item.SubItems.Add(mhs.NilaiHuruf);
+
+                lvwMahasiswa.Items.Add(item);
+            }
+        }
+
+        private void InisialisasiListView()
+        {
+            lvwMahasiswa.View = View.Details;
+            lvwMahasiswa.FullRowSelect = true;
+            lvwMahasiswa.GridLines = true;
+
+            lvwMahasiswa.Columns.Add("No ", 30, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nim ", 91, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nama", 200, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Kelas", 70, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nilai", 50, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Huruf", 50, HorizontalAlignment.Center);
+        }
+        private void ResetForm()
+        {
+            txtNim.Clear();
+            txtNama.Clear();
+            txtKelas.Clear();
+            txtNilai.Text = "0";
+            txtNim.Focus();
+        }
+        private bool NumericOnly(KeyPressEventArgs e)
+        {
+            var strValid = "0123456789";
+            if(!(e.KeyChar == Convert.ToChar(Keys.Back)))
+            {
+                // inputan selain angka
+                if(strValid.IndexOf(e.KeyChar) < 0)
+                {
+                    return true;
+                }
+                return false;
+            } 
+            else 
+                return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSimpan_Click(object sender, EventArgs e)
+        {
+            // membuat objek Mahasiswa
+            Mahasiswa mhs = new Mahasiswa();
+            mhs.Nim = txtNim.Text;
+            mhs.Nama = txtNama.Text;
+            mhs.Kelas = txtKelas.Text;  
+            mhs.Nilai = int.Parse(txtNilai.Text);
+
+            if (mhs.Nilai >= 81)
+            {
+                mhs.NilaiHuruf = "A";
+            }
+            else if (mhs.Nilai > 60)
+            {
+                mhs.NilaiHuruf = "B";
+            }
+            else if (mhs.Nilai > 40)
+            {
+                mhs.NilaiHuruf = "C";
+            }
+            else if (mhs.Nilai > 20)
+            {
+                mhs.NilaiHuruf = "D";
+            }
+            else
+            {
+                mhs.NilaiHuruf = "E";
+            }
+
+
+            list.Add(mhs);
+            var msg = "Data Mahasiswa berhasil di simpan.";
+
+            MessageBox.Show(msg, "informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ResetForm();
+        }
+
+        private void lvwMahasiswa_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        private void txtNilai_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = NumericOnly(e);
+        }
+
+        private void btnTampilkanData_Click(object sender, EventArgs e)
+        {
+            TampilkanData();
+        }
+
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            if(lvwMahasiswa.SelectedItems.Count > 0)
+            {
+                var konfirmasi = MessageBox.Show("Apakah data Mahasiswa ingin dihapus?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+                if(konfirmasi == DialogResult.Yes)
+                {
+                    var index = lvwMahasiswa.SelectedIndices[0];
+
+                    list.RemoveAt(index);
+
+                    TampilkanData();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Data Mahasiswa belum dipilih !!!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void txtNilai_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
